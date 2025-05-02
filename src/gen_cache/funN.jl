e8765cb61133891c592d8cf0ae9f6fb7ebbcaa13
Chenqi Meng <mcqgravity@gmail.com>
2025-05-02 17:36:00 +0800

    moved FunN test to test

--- src/gen_cache/funN.jl
+++ src/gen_cache/funN.jl
@@ -57,21 +57,4 @@ function FunN(R::Type{SNIrrep{N}}) where {N}
 end
 
 
-A, m, ι = FunN(S5Irrep)
 
-@planar AA_A[a123; a1 a2 a3] := m[a123; a12 a3] * m[a12; a1 a2]
-@planar A_AA[a123; a1 a2 a3] := m[a123; a1 a23] * m[a23; a2 a3]
-
-@assert isapprox(AA_A, A_AA, atol=1e-14) # check associativity
-
-@planar ιA[a; b] := m[a; x b] * ι[x]
-
-@assert isapprox(ιA, id(A), atol=1e-14) # check left unitality
-
-@planar Aι[a; b] := m[a; b x] * ι[x]
-
-@assert isapprox(Aι, id(A), atol=1e-14) # check right unitality
-
-@planar mm[a; b] := m[a; x y] * m'[x y; b]
-
-@assert isapprox(mm, id(A), atol=1e-14) # check unitarity
